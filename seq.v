cef30501501f373b4cd415e41f0e503c9506f6a3
Marcel Ullrich <ullrich.marcel@web.de>
2022-03-06 19:59:55 +0100

    progress on modulo groups

--- seq.v
+++ seq.v
@@ -66,32 +66,27 @@ Fixpoint rotate {X} n (xs:list X) :=
     end.
 
 
-Definition qrseq (q:nat) (r:nat) (Hr:r<15) : (list nat) * (list nat).
-    revert Hr.
-    pose (offset_period n :=
-        concat(repeat (rotate n period) q)
-    ).
-    refine(
+Definition qrseq (q:nat) (r:nat) : (list nat) * (list nat) :=
+    let offset_period n :=
+        concat(repeat (rotate n period) q) in
         match r with
-        |  0 => fun _ => (offset_period  0,          [])
-        |  1 => fun _ => (offset_period  0,         [1])
-        |  2 => fun _ => (offset_period  1,         [2])
-        |  3 => fun _ => (offset_period  2,         [3])
-        |  4 => fun _ => (offset_period  3,         [4])
-        |  5 => fun _ => (offset_period  4,       [3;2])
-        |  6 => fun _ => (offset_period  6,     [1;2;3])
-        |  7 => fun _ => (offset_period  9,       [4;3])
-        |  8 => fun _ => (offset_period 11,   [2;1;2;3])
-        |  9 => fun _ => (offset_period 15,     [4;3;2])
-        | 10 => fun _ => (offset_period 18,   [1;2;3;4])
-        | 11 => fun _ => (offset_period 22, [3;2;1;2;3])
-        | 12 => fun _ => (offset_period 27, [4;3;2;1;2])
-        | 13 => fun _ => (offset_period 32, [3;4;3;2;1])
-        | 14 => fun _ => (offset_period 37, [2;3;4;3;2])
-        | _ => _
-    end).
-    lia.
-Defined.
+        |  0 => (offset_period  0,          [])
+        |  1 => (offset_period  0,         [1])
+        |  2 => (offset_period  1,         [2])
+        |  3 => (offset_period  2,         [3])
+        |  4 => (offset_period  3,         [4])
+        |  5 => (offset_period  4,       [3;2])
+        |  6 => (offset_period  6,     [1;2;3])
+        |  7 => (offset_period  9,       [4;3])
+        |  8 => (offset_period 11,   [2;1;2;3])
+        |  9 => (offset_period 15,     [4;3;2])
+        | 10 => (offset_period 18,   [1;2;3;4])
+        | 11 => (offset_period 22, [3;2;1;2;3])
+        | 12 => (offset_period 27, [4;3;2;1;2])
+        | 13 => (offset_period 32, [3;4;3;2;1])
+        | 14 => (offset_period 37, [2;3;4;3;2])
+        | _ => ([],[])
+    end.
 
 Lemma rotate_sum n xs:
     sum (rotate n xs) = sum xs.
@@ -103,7 +98,7 @@ Qed.
 (* Lemma qrseq_spec q r (Hr:r<15) ps rs:
     @qrseq q r Hr = (ps,rs) -> *)
 Lemma qrseq_spec q r (Hr:r<15):
-    let (ps,rs) := @qrseq q r Hr in
+    let (ps,rs) := qrseq q r in
     sum ps = 15 * q /\
     sum rs = r.
 Proof.
@@ -123,20 +118,25 @@ Proof.
     - intros _. cbn. congruence.
 Qed.
 
-Definition n_to_seq (n:nat) : 
+Definition n_to_seq (n:nat) :=
+    let (qseq,rseq) := qrseq (n/15) (n mod 15) in
+    qseq ++ rseq.
+
+Definition n_to_seq_spec (n:nat) : 
     n>0 ->
-    {xs : list nat | sum xs = n /\ xs <> nil }.
+    let xs := n_to_seq n in
+    sum xs = n /\ xs <> nil.
 Proof.
     intros H0.
     assert(15 <> 0) as H15 by lia.
     pose proof(Nat.div_mod n 15 H15) as Hn.
     pose proof(Nat.mod_upper_bound n 15 H15) as Hr.
+    unfold n_to_seq.
     remember (n/15) as q.
     remember (n mod 15) as r.
     pose proof(@qrseq_spec q r Hr) as H.
-    destruct (@qrseq q r Hr) as [qseq rseq] eqn: Hseq.
+    destruct (@qrseq q r) as [qseq rseq] eqn: Hseq.
     destruct H as [Hqseq Hrseq].
-    exists (qseq ++ rseq).
     split.
     - now rewrite sum_app Hqseq Hrseq Hn.
     - destruct n;[lia|].
@@ -148,18 +148,6 @@ Proof.
       destruct q;[lia|].
       cbn;congruence.
 Defined.
-
-Definition n_to_stream (n:nat) : list nat.
-    remember n as m eqn:H.
-    revert H.
-    refine(match n with
-    | 0 => fun _ => []
-    | S k => fun H => _
-    end).
-    assert(m>0) as H0 by lia.
-    exact(proj1_sig (@n_to_seq m H0)).
-Defined.
-
 
 Lemma prependPeriod s:
 EqSt s seq ->
@@ -255,17 +243,140 @@ Proof.
         now rewrite <- app_assoc.
 Qed.
 
+Lemma rotate_0 {X} (xs:list X):
+    rotate 0 xs = xs.
+Proof.
+    now destruct xs;cbn.
+Qed.
+
+Lemma prepend_app {X} (xs ys:list X) s:
+    prependListStream (xs++ys) s =
+    prependListStream (xs) (prependListStream ys s).
+Proof.
+    induction xs;cbn.
+    - reflexivity.
+    - now rewrite IHxs.
+Qed.
+
+
+Lemma prependRepeatPeriod n s:
+EqSt s seq ->
+EqSt (prependListStream (concat (repeat period n)) s) seq.
+Proof.
+    intros H.
+    induction n.
+    - apply H.
+    - cbn [repeat concat].
+        rewrite prepend_app.
+        apply prependPeriod, IHn.
+Qed.
+
+
+Lemma local_mod_add n m c:
+    m <> 0 ->
+    n mod m = c ->
+    forall k,
+    k+c<m ->
+    (k+n) mod m = (k+c).
+Proof.
+    intros.
+    rewrite add_mod;[assumption|].
+    rewrite H0.
+    setoid_rewrite mod_small at 2. 2: lia.
+    rewrite mod_small. 2: lia.
+    assumption.
+Qed.
+
+
+
+(* Lemma seq_eq n:
+    n mod 15 = 2 ->
+    EqSt (concat_list n_to_seq n) (Cons 2 (Cons 3 (Cons 4 (Cons 3 (Cons 2 seq))))).
+Proof.
+    rewrite (unfold_Stream (concat_list _ _)).
+    intros Hr.
+    (* assert((S n) mod 15 = 2) as Hr by (apply (Hmod 1);lia). *)
+    cbn [concat_list].
+    unfold n_to_seq.
+    rewrite Hr.
+    cbn [qrseq].
+    remember (_ / 15) as q.
+    destruct q.
+    - cbn [repeat concat app].
+
+      cbn [prependListStream].
+      constructor;[auto|cbn [tl]].
+      admit.
+    - rewrite repeat_rotate.
+      (* Locate "_ mod _". *)
+      replace (1 mod length period) with (1) by auto.
+      cbn [drop length period take].
+      do 3 rewrite prepend_app.
+      cbn [prependListStream].
+      do 5 (constructor;[reflexivity|]);cbn [tl].
+      apply prependRepeatPeriod.
+      rewrite (unfold_Stream seq).
+      cbn [seq].
+      do 2 (constructor;[reflexivity|]);cbn [tl]. *)
+
+
 Lemma seq_eq n:
     n mod 15 = 1 ->
-    EqSt (concat_list n_to_stream n) seq.
+    EqSt (concat_list n_to_seq n) seq.
 Proof.
     revert n.
     cofix CIH.
     intros n Hn.
 
+    assert(15<>0) as H0 by lia.
+    pose proof (@local_mod_add n 15 1 H0 Hn) as Hmod.
+    clear H0.
+
+    (* 0 *)
+
     rewrite (unfold_Stream (concat_list _ _)).
     cbn.
-    unfold n_to_stream.
+    unfold n_to_seq.
+    rewrite Hn.
+    cbn [qrseq].
+    rewrite rotate_0.
+    rewrite prepend_app.
+    rewrite <- unfold_Stream.
+    apply prependRepeatPeriod.
+
+    cbn [prependListStream].
+    rewrite (unfold_Stream seq).
+    cbn [seq].
+    
+    constructor;[auto|].
+    cbn [tl].
+    (* rewrite (unfold_Stream (Cons _ _)). *)
+
+
+    rewrite (unfold_Stream (concat_list _ _)).
+    assert((S n) mod 15 = 2) as Hr by (apply (Hmod 1);lia).
+    cbn [concat_list].
+    rewrite Hr.
+    cbn [qrseq].
+    remember (_ / 15) as q.
+    destruct q.
+    - cbn [repeat concat app].
+
+      cbn [prependListStream].
+      constructor;[auto|cbn [tl]].
+      admit.
+    - rewrite repeat_rotate.
+      (* Locate "_ mod _". *)
+      replace (1 mod length period) with (1) by auto.
+      cbn [drop length period take].
+      do 3 rewrite prepend_app.
+      cbn [prependListStream].
+      do 5 (constructor;[reflexivity|]);cbn [tl].
+      apply prependRepeatPeriod.
+      rewrite (unfold_Stream seq).
+      cbn [seq].
+      do 2 (constructor;[reflexivity|]);cbn [tl].
+      
 
 Qed.
 
